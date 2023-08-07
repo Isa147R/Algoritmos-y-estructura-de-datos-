a24cc161cd45f418879f2d5d81da1135288b01a0
Isa147R <131714039+Isa147R@users.noreply.github.com>
2023-08-07 15:38:38 -0500

    ClaseVector.cpp

--- ClaseVector.cpp
+++ ClaseVector.cpp
@@ -1,92 +1,100 @@
-#include iostream;
+#include <iostream>
 using namespace std;
 
-template class Vector{
+template <typename T>
+class Vector {
 private:
-T* v;
-int size;
-int capacidad;
+    T* v;
+    int size;
+    int capacidad;
 
 public:
-Vector() {
-capacidad = 10;
-size = 0;
-v = new T[capacidad];
+    Vector() {
+        capacidad = 10;
+        size = 0;
+        v = new T[capacidad];
+    }
 
-}
+    Vector(int c) {
+        capacidad = c;
+        size = 0;
+        v = new T[capacidad];
+    }
 
-Vector(int c) {
-capacidad = c;
-size = 0;
-v = new T[capacidad];
+    ~Vector() {
+        delete[] v;
+    }
 
-}
+    void add(T valor) {
+        if (size == capacidad) {
+            incrementar_capacidad();
+        }
+        v[size++] = valor;
+    }
 
-~Vector() {
-delete[] v;
-}
+    void print() {
+        for (int i = 0; i < size; ++i) {
+            cout << v[i] << " ";
+        }
+        cout << endl;
+    }
 
-void add(T valor){
-if (size == capacidad) {
-incrementar_capacidad();
-}
-v[size++] = valor;
-}
+    T get(int x) {
+        return v[x];
+    }
 
-void print(){
-for (int i = 0; i < size; ++i){
-cout << v[i] << endl;
-}
-}
+    void incrementar_capacidad() {
+        T* v1 = new T[capacidad *= 2];
+        for (int i = 0; i < size; ++i) {
+            v1[i] = v[i];
+        }
+        delete[] v;
+        v = v1;
+    }
 
-T get (int x){
-return v[x];
-}
+    void set(T dato, int p) {
+        v[p] = dato;
+    }
 
-void incrementar_capacidad(){
-T* v1 = new T[capacidad*=2];
-for (int i = 0; i < size; ++i){
-v1[i]=v[i];
-}
-delete[] v;
-v = v1;
-}
-void insert(T dato, int p){
-if (size == capacidad) {
-incrementar_capacidad();
-v[p]=dato;
-size++;
-}
-v[p] = dato;
-}
+    void insert(T dato, int p) {
+        if (size == capacidad) {
+            incrementar_capacidad();
+        }
+        for (int i = size; i > p; --i) {
+            v[i] = v[i - 1];
+        }
+        v[p] = dato;
+        size++;
+    }
 
-void set(T dato, int p){
-v[p] = dato;
-}
-
+    void remove(int p) {
+        for (int i = p; i < size - 1; ++i) {
+            v[i] = v[i + 1];
+        }
+        size--;
+    }
 };
 
-int main()
-{
-Vector vector1;
-for (int i = 0; i < 5; i++){
-vector1.add(i);
-}
-Vector vector2;
-for (int i = 0; i < 5; i++){
-vector2.add(i);
-}
+int main() {
+    Vector<int> vector1;
+    for (int i = 0; i < 5; i++) {
+        vector1.add(i);
+    }
 
-cout << "Vector Normal " << endl;
-vector1.print();
+    cout << "Vector Normal: ";
+    vector1.print();
 
-cout << "Metodo set" << endl;
-vector1.set(9,2);
-vector1.print();
+    cout << "Metodo set" << endl;
+    vector1.set(9, 2);
+    vector1.print();
 
+    cout << "Metodo insert" << endl;
+    vector1.insert(4, 2);
+    vector1.print();
 
-cout << "Metodo insert" << endl;
-vector2.insert(9,2);
-vector2.print();
-return 0;
+    cout << "Metodo remove" << endl;
+    vector1.remove(1);
+    vector1.print();
+
+    return 0;
 }
